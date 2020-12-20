16e17ee366d325bab014da900e2aa2dd6e6af2d3
ogneyar <ogneyar-ne@ya.ru>
2020-12-20 10:47:07 +0300

    теперь работает гет запрос

--- getMethod.cpp
+++ getMethod.cpp
@@ -11,15 +11,17 @@ static char Rec[2097152]; // 2Mb
 
 int main() {
     
+    // char url[] = "https://hutoryanin.ru/test";
+
     // char url[] = "http://hutoryanin.ru/test?id=this&name=haski";
 
     // char url[] = "http://localhost:8000/shop/room?id=this&name=haski";
 
-    // char url[] = "http://localhost:8000/test";
+    char url[] = "http://localhost:8000/test";
 
     // char url[] = "https://youtube.com?ggg=222";
 
-    char url[] = "youtube.com?ggg=222";
+    // char url[] = "https://youtube.com";
     
     cout << "Start parser url. Url: " << url << endl << endl; 
     
@@ -65,77 +67,110 @@ int main() {
     }
 
 
-    // char hooost[1024] = "hutoryanin.ru";
-    // cout << "gethostbyname: " <<  inet_ntoa(*((in_addr*)gethostbyname(hooost)->h_addr_list[0])) << endl;
+    // // char hooost[1024] = "hutoryanin.ru";
+    // // cout << "gethostbyname: " <<  inet_ntoa(*((in_addr*)gethostbyname(hooost)->h_addr_list[0])) << endl;
 
 
 
-    struct addrinfo* addr = NULL; // структура, хранящая информацию
-    // об IP-адресе  слущающего сокета
+    // struct addrinfo* addr = NULL; // структура, хранящая информацию
+    // // об IP-адресе  слущающего сокета
 
-    // Шаблон для инициализации структуры адреса
-    struct addrinfo hints;
-    ZeroMemory(&hints, sizeof(hints));
+    // // Шаблон для инициализации структуры адреса
+    // struct addrinfo hints;
+    // ZeroMemory(&hints, sizeof(hints));
 
-    // AF_INET определяет, что используется сеть для работы с сокетом
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM; // Задаем потоковый тип сокета
-    hints.ai_protocol = IPPROTO_TCP; // Используем протокол TCP
-    // Сокет биндится на адрес, чтобы принимать входящие соединения
-    hints.ai_flags = AI_PASSIVE;
+    // // AF_INET определяет, что используется сеть для работы с сокетом
+    // hints.ai_family = AF_INET;
+    // hints.ai_socktype = SOCK_STREAM; // Задаем потоковый тип сокета
+    // hints.ai_protocol = IPPROTO_TCP; // Используем протокол TCP
+    // // Сокет биндится на адрес, чтобы принимать входящие соединения
+    // hints.ai_flags = AI_PASSIVE;
     
     
-    // Инициализируем структуру, хранящую адрес сокета - addr.
-    // HTTP-сервер будет висеть на заданном порту заданного хоста
-    result = getaddrinfo(host, "80", &hints, &addr);
-    // Если инициализация структуры адреса завершилась с ошибкой,
-    // выведем сообщением об этом и завершим выполнение программы 
-    if (result != 0) {
-        cerr << "getaddrinfo failed: " << result << "\n";
-        WSACleanup(); // выгрузка библиотеки Ws2_32.dll
-        system("pause");
-        return 1;
-    }
+    // // Инициализируем структуру, хранящую адрес сокета - addr.
+    // // HTTP-сервер будет висеть на заданном порту заданного хоста
+    // result = getaddrinfo(host, "80", &hints, &addr);
+    // // Если инициализация структуры адреса завершилась с ошибкой,
+    // // выведем сообщением об этом и завершим выполнение программы 
+    // if (result != 0) {
+    //     cerr << "getaddrinfo failed: " << result << "\n";
+    //     WSACleanup(); // выгрузка библиотеки Ws2_32.dll
+    //     system("pause");
+    //     return 1;
+    // }
 
 
-    // Создание сокета
-    int listen_socket = socket(addr->ai_family, addr->ai_socktype,
-        addr->ai_protocol);
+    // // Создание сокета
+    // int listen_socket = socket(addr->ai_family, addr->ai_socktype,
+    //     addr->ai_protocol);
 
-    // int listen_socket = socket(AF_INET, SOCK_STREAM, 0);
+    // // int listen_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
 
-    // Если создание сокета завершилось с ошибкой, выводим сообщение,
-    // освобождаем память, выделенную под структуру addr,
-    // выгружаем dll-библиотеку и закрываем программу
-    if (listen_socket == INVALID_SOCKET) {
-        cerr << "Error at socket: " << WSAGetLastError() << "\n";
-        freeaddrinfo(addr);
-        WSACleanup();
-        system("pause");
-        return 1;
-    }
+    // // Если создание сокета завершилось с ошибкой, выводим сообщение,
+    // // освобождаем память, выделенную под структуру addr,
+    // // выгружаем dll-библиотеку и закрываем программу
+    // if (listen_socket == INVALID_SOCKET) {
+    //     cerr << "Error at socket: " << WSAGetLastError() << "\n";
+    //     freeaddrinfo(addr);
+    //     WSACleanup();
+    //     system("pause");
+    //     return 1;
+    // }
 
-    // bind(listen_socket, (sockaddr *)&addr, sizeof(addr));
+    // // bind(listen_socket, (sockaddr *)&addr, sizeof(addr));
 
+	// // соединяемся с сервером
+	// if(connect(listen_socket, (sockaddr *)&addr, sizeof(addr)) != 0) {
+	// 	cout << "Error: failed connect to server.\n" << endl;
+	// 	system("pause");
+	// 	return 1;
+	// }
+	// cout << "Connected in the server.\n" << endl;
+
+
+
+
+
+    SOCKADDR_IN addr;
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr(ip);
+	addr.sin_port = htons(port);
+	addr.sin_family = AF_INET;
+
+	// создаём сокет
+	int listen_socket = socket(AF_INET, SOCK_STREAM, 0);
 	// соединяемся с сервером
-	if(connect(listen_socket, (sockaddr *)&addr, sizeof(addr)) != 0) {
+	if(connect(listen_socket, (SOCKADDR*)&addr, sizeof(addr)) != 0) {
 		cout << "Error: failed connect to server.\n" << endl;
 		system("pause");
 		return 1;
 	}
 	cout << "Connected in the server.\n" << endl;
+
+
+
+
+
     
     char query[2048];
     const char * user_agent = "Search Engine";
+    // const char * user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/68.0.3440.75 Chrome/68.0.3440.75 Safari/537.36";
 
-    strcpy( query, "GET /" );
+
+    const char * accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
+
+    strcpy( query, "GET " );
     strcat( query, path );
-    strcat( query, " HTTP/1.0\nHost: ");
+    strcat( query, " HTTP/1.1\nHost: ");
     strcat( query, host);
     strcat( query, "\nUser-agent: ");
+    // strcat( query, "\nConnection: keep-alive\nUser-agent: ");
     strcat( query, user_agent);
     strcat( query, "\nAccept: */*\n\n");
+    // strcat( query, "\nAccept: ");
+    // strcat( query, accept);
+    // strcat( query, "\n\n");
 
     int cnt = send(listen_socket, query, strlen(query), 0);
 
@@ -145,7 +180,6 @@ int main() {
 		return 1;
 	}
 
-   
     char InBuff[2048];
 
     strcpy(Rec, "");
@@ -156,9 +190,14 @@ int main() {
         // receive a 2048 bytes from 's' sock
         cnt = recv (listen_socket, (char*)&InBuff, sizeof(InBuff),0);
         // append it to main buffer 'Rec'
-        strcat(Rec,InBuff);
+        strcat( Rec, InBuff );
     }
     char* str = (char*)&Rec;
+
+
+
+    // char* str;
+    // recv (listen_socket, (char*)str, sizeof(str),0);
 
     cout << "Response: " << endl << str << endl << endl;;
 
@@ -169,7 +208,7 @@ int main() {
 
     // Убираем за собой
     closesocket(listen_socket);
-    freeaddrinfo(addr);
+    // freeaddrinfo(addr);
     WSACleanup();
 
 
--- tests/web_server_http/main.cpp
+++ tests/web_server_http/main.cpp
@@ -292,7 +292,7 @@ void send_404(int aSock)
 // Connection: keep-alive
 // Upgrade-Insecure-Requests: 1
 // User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/68.0.3440.75 Chrome/68.0.3440.75 Safari/537.36
-// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
+// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8 
 // Accept-Encoding: gzip, deflate, br
 // Accept-Language: en-US,en;q=0.9,ru;q=0.8
 
