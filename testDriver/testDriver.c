603dafc178ab6a3d675cf49ce53c09f3fa40fcce
ogneyar <ogneyar-ne@ya.ru>
2021-01-05 16:53:14 +0300

    sss

---
+++ testDriver/testDriver.c
@@ -0,0 +1,22 @@
+
+#include <ntddk.h>
+ 
+NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING RegistryPath);
+VOID UnloadRoutine(IN PDRIVER_OBJECT DriverObject);
+ 
+#pragma alloc_text(INIT, DriverEntry)
+#pragma alloc_text(PAGE, UnloadRoutine)
+ 
+NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING RegistryPath)
+{
+  DriverObject->DriverUnload = UnloadRoutine;
+ 
+  DbgPrint(«Hello world!\n»);
+ 
+  return STATUS_SUCCESS;
+}
+ 
+VOID UnloadRoutine(IN PDRIVER_OBJECT DriverObject)
+{
+  DbgPrint(«Goodbye!\n»);
+}
